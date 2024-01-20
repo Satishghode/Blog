5627ccba7fc0798c0b6a88710b4797a3aa187bce
Satishghode <satishghode9075@gmail.com>
2024-01-20 15:58:49 +0530

    create the pages and set routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 
 function App() {
   return (
-    <div className='' >App</div>
-  )
+    <BrowserRouter >
+       <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/project' element={<Project />} />
+       </Routes>
+    </BrowserRouter>
+
+    )
 }
 
 export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>hey this side home pages lets go and bulid an home pages </h1>
+    </div>
+  )
+}
+
+export default Home
