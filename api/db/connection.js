3cb743aa77f266c301eabd9549734a9e216718b5
Satishghode <satishghode9075@gmail.com>
2024-01-20 23:02:01 +0530

    connect to the database

---
+++ api/db/connection.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const connectDB = async () =>{
+    try {
+        const connectionInatance = await mongoose.connect(process.env.MONGOOSE_URL)
+        console.log("DB Connected")
+    } catch (error) {
+        console.log(` Mongoose connection error :  ${error}`);
+    }
+
+}
+
+export default connectDB;
+
+
+
+//  this is second way to connect to the database server and connect to the database server.
+    
+
+/*
+mongoose.connect(process.env.MONGOOSE_URL)
+ .then(() => {
+    console.log("database connection established");
+ })
+ .catch ((error) => {
+    console.log(`database connection error: ${ error } `);
+ })
+
+*/
--- api/index.js
+++ api/index.js
@@ -1,15 +1,37 @@
 import express from 'express';
-import { env } from 'process';
+import connectDB from './db/connection.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+ 
+
+dotenv.config();
+
 
 const app = express();
 
-const port = env.port;
+// establishing a database connection to the server and connect to the database server.
 
-app.listen( env.port ||  3000 , () => {
-    console.log("server is listening on port 3000!!!")
+connectDB()
+.then(() => {
+    console.log("connection established");
+})
+.catch((error) =>{
+    console.log(`this is DB connection error ${error}`); 
+})
+
+//  ROUTRES
+
+app.listen(process.env.PORT || 3000 , () => {
+    console.log(`server is listening on port  ${process.env.PORT} `  )
 });
+
+
 
 app.get('/',(req, res) => {
 
     res.send("Server is on home page");
 })
+
+
+
+
